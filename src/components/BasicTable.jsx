bf05ef996ce545e5abfe37a6cb4fc6c0687b41e2
lvanish <lvanishkoul@gmail.com>
2024-01-18 01:47:26 +0530

    Revert "Updating dob with luxon and changing to accessorfn for name"
    This reverts commit 78dcc86d935aaccb7c905c07a00cc2e73f415dc8.

--- src/components/BasicTable.jsx
+++ src/components/BasicTable.jsx
@@ -1,6 +1,5 @@
 import {useReactTable, flexRender, getCoreRowModel} from '@tanstack/react-table'
 import mData from '../MOCK_DATA.json'
-import {DateTime} from 'luxon'
 import { useMemo } from 'react'
 export default function BasicTable() {
 
@@ -14,8 +13,14 @@ export default function BasicTable() {
       footer: 'ID',
     },
     {
-      header: 'Name',
-      accessorFn: row => `${row.first_name} ${row.last_name}`
+      header: 'First name',
+      accessorKey: 'first_name',
+      footer: 'First name',
+    },
+    {
+      header: 'Last name',
+      accessorKey: 'last_name',
+      footer: 'Last name',
     },
     {
       header: 'Email',
@@ -31,7 +36,6 @@ export default function BasicTable() {
       header: 'Date of birth',
       accessorKey: 'dob',
       footer: 'Date of birth',
-      cell: info => DateTime.fromISO(info.getValue()).toLocaleString(DateTime.DATE_MED),
     },
   ]
 
