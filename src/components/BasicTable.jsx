202c6d1142a13213ad22c03d6f027548af3c7f6b
lvanish <lvanishkoul@gmail.com>
2024-01-28 02:51:00 +0530

    Adding filtering, pagination and sorting

--- src/components/BasicTable.jsx
+++ src/components/BasicTable.jsx
@@ -1,6 +1,7 @@
-import {useReactTable, flexRender, getCoreRowModel} from '@tanstack/react-table'
+import {useReactTable, flexRender, getCoreRowModel, getPaginationRowModel, getSortedRowModel, getFilteredRowModel} from '@tanstack/react-table'
 import mData from '../MOCK_DATA.json'
-import { useMemo } from 'react'
+import { useMemo, useState } from 'react'
+import {DateTime} from 'luxon'
 export default function BasicTable() {
 
   const data = useMemo(() => mData, [])
@@ -36,25 +37,43 @@ export default function BasicTable() {
       header: 'Date of birth',
       accessorKey: 'dob',
       footer: 'Date of birth',
+      cell: info=> DateTime.fromISO(info.getValue()).toLocaleString(DateTime.DATE_MED)
     },
   ]
-
-
-  const table = useReactTable({data, columns, getCoreRowModel: getCoreRowModel()})
+  const [sorting, setSorting] = useState([])
+  const [filtering, setFiltering] = useState('')
+  const table = useReactTable({
+    data, 
+    columns, 
+    getCoreRowModel: getCoreRowModel(), 
+    getPaginationRowModel: getPaginationRowModel(), 
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    state: {
+      sorting: sorting,
+      globalFilter: filtering
+    },
+    onSortingChange: setSorting,
+    onGlobalFilterChange: setFiltering,
+  })
 
   return (
     <div className="overflow-x-auto pt-6">
+      <input type='text' value={filtering} onChange={e => setFiltering(e.target.value)}/>
       <table className='table'>
         <thead className="bg-base-200 ">
           {table.getHeaderGroups().map(x => (
             <tr key={x.id}>
               {x.headers.map(head => (
-                <th key={head.id}>
+                <th key={head.id} onClick={head.column.getToggleSortingHandler()}>
                   {
                     flexRender(
                       head.column.columnDef.header,
                       head.getContext()
                     )
+                  }
+                  {
+                    {asc:'⬆️', desc:'⬇️'}[head.column.getIsSorted() ?? null]
                   }
                 </th>
               ))}
@@ -77,23 +96,13 @@ export default function BasicTable() {
             </tr>
           ))}
         </tbody>
-        <tfoot>
-          {table.getFooterGroups().map(x => (
-            <tr key={x.id}>
-              {x.headers.map(head => (
-                <th key={head.id}>
-                  {
-                    flexRender(
-                      head.column.columnDef.header,
-                      head.getContext()
-                    )
-                  }
-                </th>
-              ))}
-            </tr>
-          ))}
-        </tfoot>
       </table>
+      <div>
+        <button className='btn' onClick={() => table.setPageIndex(0)}>First Page</button>
+        <button className='btn' disabled={!table.getCanPreviousPage()} onClick={() => table.previousPage()}>Previous Page</button>
+        <button className='btn' disabled={!table.getCanNextPage()} onClick={() => table.nextPage()}>Next Page</button>
+        <button className='btn' onClick={() => table.setPageIndex(table.getPageCount() - 1)}>Last Page</button>
+      </div>
     </div>
   )
 }
